4a53a01f160a1050e0549453db76746c5552102e
Bina Duwal <duwalbina2@gmail.com>
2025-06-12 10:17:18 +0545

    Added BasicScript1 test and update BrowserTest implementation

--- src/test/java/BrowserTest.java
+++ src/test/java/BrowserTest.java
@@ -1,14 +1,3 @@
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.firefox.FirefoxDriver;
-//
-//
-//public class BrowserTest {
-//
-//	public static void main(String[] args) {
-//		WebDriver driver=new FirefoxDriver();
-//		driver.get();}
-//}
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
